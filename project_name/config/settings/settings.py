66efe9bbe9d6ebc641bc502ca2f0765ea9a2c733
jonathan <jony327@gmail.com>
2016-12-26 16:43:59 -0500

    initial

---
+++ project_name/config/settings/settings.py
@@ -0,0 +1,120 @@
+"""
+Django settings for dj_gen project.
+"""
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+import environ
+
+PROJECT_ROOT = environ.Path(__file__) - 3
+APPS_DIR = PROJECT_ROOT.path('apps')
+PROJECT_TEMPLATES = [
+    str(PROJECT_ROOT.path('templates')),
+]
+env = environ.Env()
+
+SECRET_FILE = str(PROJECT_ROOT.path('security/SECRET.key'))
+try:
+    from django.utils.crypto import get_random_string
+
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!$%&()=+-_'
+    SECRET_KEY = get_random_string(50, chars)
+    with open(SECRET_FILE, 'w') as f:
+        f.write(SECRET_KEY)
+        f.close()
+except IOError:
+    raise Exception('Could not open %s for writing!' % SECRET_FILE)
+
+SECRET_KEY = 'zmji3zc+k_(&k+onhkl!if3z+dr)4bp-g_vye5v369532+prma'
+
+ALLOWED_HOSTS = []
+
+# Application definition
+DJANGO_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+LOCAL_APPS = (
+
+)
+THIRD_PARTY_APPS = (
+
+)
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
+
+MIDDLEWARE = (
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'project_name.config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'project_name.config.wsgi.application'
+
+# Database
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, '../security/db.sqlite3'),
+#     }
+# }
+
+# Password validation
+#
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = False
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
