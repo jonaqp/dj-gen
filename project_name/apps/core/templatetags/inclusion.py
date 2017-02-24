80d9888633faa0cda1aba6272d2a7687394fe774
jonathan <jony327@gmail.com>
2017-02-24 17:01:57 -0500

    adding gitignore

--- project_name/apps/core/templatetags/inclusion.py
+++ project_name/apps/core/templatetags/inclusion.py
@@ -9,6 +9,7 @@ register = template.Library()
 
 @register.inclusion_tag('themes/partials/siderbar-menu.html', takes_context=True)
 def tag_menu_siderbar(context):
+    """for order need change line 22, 29, 34"""
     request = context['request']
     user = User.objects.get(email=request.user)
     module_team = ModuleTeam.current.filter(
@@ -18,23 +19,22 @@ def tag_menu_siderbar(context):
     for x in module_team.iterator():
         m_team = ModuleTeam.current.get(module=x["module"])
         if m_team.module.name not in result.keys():
-            result[m_team.module.order] = list()
+            result[m_team.module.name] = list()
         add_module_dict = dict(
             module=dict(
                 text=m_team.module.name, match=m_team.module.match,
                 submodule=dict()
             )
         )
-        result[m_team.module.order].append(add_module_dict)
+        result[m_team.module.name].append(add_module_dict)
         m_itemteam = ModuleItemTeam.current.filter(
             module_team=m_team)
         if m_itemteam.exists():
             for y in m_itemteam:
-                dict_sub_menu = result[m_team.module.order][0]['module']['submodule']
+                dict_sub_menu = result[m_team.module.name][0]['module']['submodule']
                 if y.moduleitem.reference not in dict_sub_menu.keys():
                     dict_sub_menu[y.moduleitem.reference] = list()
                 add_module_dict = dict(text=y.moduleitem.name,
                                        match=y.moduleitem.match)
                 dict_sub_menu[y.moduleitem.reference].append(add_module_dict)
-    print(result)
     return {'result': result}
